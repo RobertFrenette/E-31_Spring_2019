ab0810b818b50fbb64cd88be33197bcfd4b4c471
Robert Frenette <robert.frenette@comcast.net>
2019-01-30 09:21:27 -0500

    Added UNIX command comments.

--- stringserver.js
+++ stringserver.js
@@ -1,3 +1,8 @@
+// nohup node stringserver &
+// http://68.183.107.205:8080/E-57a_Spring_2019/stringserver.js
+// ps aux | grep node
+// EX: root      1592  0.3  2.8 922536 29228 ?        Sl   14:17   0:00 node stringserver
+// kill 1592 
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
